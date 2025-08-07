87b1b6c961a75402f250cd5ff2640d75653c60c3
Paliy <makspalij1@gmail.com>
2025-08-07 14:40:35 +0300

    first commit

---
+++ src/app-module.ts
@@ -0,0 +1,15 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { TaskNoteComponent } from './task-note/task-note.component';
+import { PlaygroundComponent } from './game/PlaygroundComponent';
+import { A11yModule } from '@angular/cdk/a11y';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [PlaygroundComponent, TaskNoteComponent],
+  imports: [BrowserModule, CommonModule, MatSlideToggleModule, A11yModule],
+  providers: [],
+  bootstrap: [PlaygroundComponent],
+})
+export class AppModule {}
---
+++ src/task-note/task-note.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-note',
+  templateUrl: './task-note.component.html',
+  styleUrls: ['./task-note.component.css'],
+})
+export class TaskNoteComponent {}
